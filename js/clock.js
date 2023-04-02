ed579f211a24fa3e3a73f67d4d1f2ae2e44aba11
ajin <aj5210@naver.com>
2023-04-02 19:39:47 +0900

    feature todo

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+// setInterval(sayHello, 5000); // 5초 마다 실행
+//setTimeout(sayHello, 5000); //5초 뒤에 실행
